65b057c2f6a090ecf9b3b85474d417021966aee7
Daniel Barros <daniel_barros_silva@hotmail.com>
2018-02-09 14:19:11 -0200

    Updated code and interface.
    Created a floating window for profile showing, created a class for the main view control buttons' OnClickListener and started to test the RTDataBase communication.

--- java/net/net16/httpschaos_workbench/rpgchat/LoginActivity.java
+++ java/net/net16/httpschaos_workbench/rpgchat/LoginActivity.java
@@ -1,20 +1,19 @@
 package net.net16.httpschaos_workbench.rpgchat;
 
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatDialog;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
@@ -30,25 +29,35 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
 
     public static int RC_SIGN_IN = 0;
 
     private GoogleSignInClient mGoogleSignInClient;
     private AppCompatDialog dialog;
     private FirebaseAuth mAuth;
-    private ArrayAdapter<LinearLayout> contactAdpter;
-    private ListView contactList;
+    private Drawable profilePic;
+    private ImageView profileImage2;
+    private ImageButton imageView0, imageView1, imageView2, imageView3, imageView4;
+    private TextView profileName, profileStatus;
+    private EditText nameEditor, statusEditor;
+    private Button nameEditorOk, statusEditorOk;
+    private LinearLayout nameLayout, statusLayout;
+    private FirebaseUser user;
+
     AlertDialog alertDialog;
-    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        contactList = findViewById(R.id.listView0);
+        ImageView profileImage1 = findViewById(R.id.profileImage);
+        profileName = findViewById(R.id.profileName);
+        profileStatus = findViewById(R.id.profileStatus);
 
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -74,7 +83,7 @@ public class LoginActivity extends AppCompatActivity {
         user = mAuth.getCurrentUser();
         if (account != null) {
             dialog.dismiss();
-            loginSuccesfull(user);
+            loginSuccesfull();
         } else {
             dialog.setContentView(R.layout.login);
             dialog.show();
@@ -86,6 +95,16 @@ public class LoginActivity extends AppCompatActivity {
         startActivityForResult(signInIntent, RC_SIGN_IN);
         dialog.setContentView(R.layout.progress_show);
         dialog.show();
+        imageView0 = findViewById(R.id.imageButton0);
+        imageView1 = findViewById(R.id.imageButton1);
+        imageView2 = findViewById(R.id.imageButton2);
+        imageView3 = findViewById(R.id.imageButton3);
+        imageView4 = findViewById(R.id.imageButton4);
+        imageView0.setOnClickListener(new ControlButtonsOnClickListener());
+        imageView1.setOnClickListener(new ControlButtonsOnClickListener());
+        imageView2.setOnClickListener(new ControlButtonsOnClickListener());
+        imageView3.setOnClickListener(new ControlButtonsOnClickListener());
+        imageView4.setOnClickListener(new ControlButtonsOnClickListener());
     }
 
     @Override
@@ -116,7 +135,7 @@ public class LoginActivity extends AppCompatActivity {
                                     .setPositiveButton("OK", null)
                                     .show();
                             user = mAuth.getCurrentUser();
-                            loginSuccesfull(user);
+                            loginSuccesfull();
                         } else {
                             // If sign in fails, display a message to the user.
                             alertDialog = new AlertDialog.Builder(getApplicationContext())
@@ -130,14 +149,45 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
 
-    private void loginSuccesfull(FirebaseUser user) {
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(user.getDisplayName());
-        ImageView imageView = findViewById(R.id.profileImage);
-        imageView.setImageURI(user.getPhotoUrl());
+    private void loginSuccesfull() {
+        UserValueEventListener nameValueEventListener = new UserValueEventListener(user, profileName);
+        UserValueEventListener statusValueEventListener = new UserValueEventListener(user, profileStatus);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference statusReference = database.getReference("Status");
+        statusReference.addValueEventListener(statusValueEventListener);
+        DatabaseReference nameReference = database.getReference("Usuario");
+        nameReference.addValueEventListener(nameValueEventListener);
     }
 
-    public Context getContext() {
-        return this;
+    public void openProfile(MenuItem i) {
+        dialog.setContentView(R.layout.profile_view);
+        dialog.show();
+        profileImage2 = findViewById(R.id.profileImage2);
+        profileImage2.setImageDrawable(profilePic);
+        profileName.setText(user.getDisplayName());
+    }
+
+    public void openAbout(MenuItem i) {
+    }
+
+    public void logout(MenuItem i) {
+    }
+
+    public void setProfilePic(Drawable pic) {
+        profilePic = pic;
+    }
+
+    public void changeProfileName(View v) {
+    }
+
+    public void changeStatus(View v) {
+    }
+
+    private void errMessage(String msg) {
+        alertDialog = new AlertDialog.Builder(this)
+                .setTitle("Erro:")
+                .setMessage(msg)
+                .setPositiveButton("Ok", null)
+                .show();
     }
 }
--- java/net/net16/httpschaos_workbench/rpgchat/MainMenuActions.java
+++
@@ -1,8 +0,0 @@
-package net.net16.httpschaos_workbench.rpgchat;
-
-/**
- * Created by Daniel on 31/01/2018.
- */
-
-public class MainMenuActions {
-}
